9fa9919e661177e3be932c9150639abae5686609
Harsh Panchal <harshhhh1803@gmail.com>
2022-05-01 12:02:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/systeminfo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.systeminfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("System Information");
+
+        TextView sysversion=(TextView)findViewById(R.id.sysversion);
+        sysversion.setText(System.getenv("os.version"));
+
+        TextView osname=(TextView)findViewById(R.id.osname);
+        osname.setText(System.getProperty("os.name"));
+
+        TextView osarch=(TextView)findViewById(R.id.arch);
+        osarch.setText(System.getProperty("os.arch"));
+
+        TextView manufacturer=(TextView)findViewById(R.id.manufacturer);
+        manufacturer.setText(android.os.Build.MANUFACTURER);
+    }
+}
